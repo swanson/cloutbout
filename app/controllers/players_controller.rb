d6175086c986b1f07d093645d7e1d5d15aa7a8fa
Matt Swanson <mdswanson@sep.com>
2011-08-27 20:32:11 -0500

    spacing issues

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,9 +4,9 @@ class PlayersController < ApplicationController
   end
 
   def set_score
-      player = Player.where(:name => params['name']).first
-      player.current_score = params['score'].to_f
-      player.save
+    player = Player.where(:name => params['name']).first
+    player.current_score = params['score'].to_f
+    player.save
   end
 
 end
